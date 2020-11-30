948c75573f19458fc049dd904c78c1e46e9faf75
Joshua Chin <joshuachin191@gmail.com>
2020-11-30 14:44:57 +0800

    Refactored code into modules

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const http = require('http');
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const notesRouter = require('./controllers/blogs.js');
+const cors = require('cors');
 require('dotenv').config();
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = process.env.MONGODB_URI;
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
@@ -24,23 +17,7 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, us
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', notesRouter);
 
 const PORT = 3003
 app.listen(PORT, () => {
