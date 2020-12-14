65281493d9c028753f6dcef47d91ef6c37cec906
Joshua Chin <joshuachin191@gmail.com>
2020-12-14 11:32:02 +0800

    Fixed bug, added in async/await

--- controllers/testing.js
+++ controllers/testing.js
@@ -2,9 +2,9 @@ const testRouter = require('express').Router();
 const Blog = require('../models/blogs');
 const User = require('../models/users');
 
-testRouter.post('/reset', (req, res) => {
-  Blog.deleteMany({});
-  User.deleteMany({});
+testRouter.post('/reset', async (req, res) => {
+  await Blog.deleteMany({});
+  await User.deleteMany({});
   res.status(204).end();
 });
 
